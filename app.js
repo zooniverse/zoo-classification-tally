fbe781732198f93a1c39df762ee7a769e95fc82b
Rona Costello <rona.costello@plants.ox.ac.uk>
2018-09-26 13:23:57 +0100

    Create app.js to listen for pusher events
    Currently this app listens to the Panoptes event bus from Pusher and for
    any classification events it prints the event data to the console.

---
+++ app.js
@@ -0,0 +1,25 @@
+// Enable pusher logging - don't include this in production
+// Pusher.logToConsole = true;
+
+var PusherProdKey = '79e8e05ea522377ba6db';
+var pusherStagKey = '95781402b5854a712a03';
+var pusherDevKey = '95781402b5854a712a03';
+
+var pusher = new Pusher(PusherProdKey);
+var panoptesChannel = pusher.subscribe('panoptes');
+
+// This code runs each time a classification event comes down
+// the panoptes pusher pipe
+panoptesChannel.bind('classification', function(data) {
+   console.log(data);
+});
+
+// userKey: "user:1804243"
+// var pusher = new Pusher('51aa9c226f414d8b1a32', {
+//   cluster: 'eu',
+//   forceTLS: true
+// });
+//
+// var channel = pusher.subscribe('my-channel');
+// channel.bind('my-event', function(data) {
+//   alert(JSON.stringify(data));
