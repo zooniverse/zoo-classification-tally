490e890758151654c1a7fb463f227b818fa5d64e
Rona Costello <rona.costello@plants.ox.ac.uk>
2018-10-04 19:53:51 +0100

    Declare a global count variable
    I wanted to fetch the start classification count and then add to it
    incremntally. As I have moved everything into async functions, I have
    started a global count var so that this can be accessed at all times to
    be added to and written to the appropriate elements on the page.
    
    This is a work in progress but at this point the project&user count (in
    the centre of the page) works.

--- src/index.js
+++ src/index.js
@@ -9,50 +9,125 @@ var pusherDevKey = '95781402b5854a712a03';
 var pusher = new Pusher(PusherProdKey);
 var panoptesChannel = pusher.subscribe('panoptes');
 
-var projectID = 3434;
-// var projectID = 4996 ss
+// console.log(window.location.search);
 
-var Count = 0
+var projectID = 4996;
+var userID = 1804243;
 
-console.log(window.location.search);
+window.appData = {  //Declare a global variable. Not really a great idea, but hey.
+  userCount: 0
+};
 
-var getClassificationCount = function(url) {
-  $.getJSON(url, function(data) {
-    var total_classifications = 0;
-    var stats_data = data.events_over_time.buckets;
-    for (var i in stats_data) {
-      total_classifications = total_classifications + parseInt(stats_data[i].doc_count);
+// Get the current count values and start count
+var userQueryURL = "http://stats.zooniverse.org/counts/classification/year?project_id=4996&user_id=1804243";
+var projectQueryURL = "http://stats.zooniverse.org/counts/classification/year?project_id=4996&user_id=1804243";
+
+setStartCount(userQueryURL, "#counter");
+
+function setStartCount(userQueryURL, container) {
+  $.getJSON(userQueryURL, function(data) {
+    var totalClassifications = 0;
+    var statData = data.events_over_time.buckets;
+    for (var i in statData) {
+      totalClassifications = totalClassifications + parseInt(statData[i].doc_count);
     };
-    return(total_classifications);
+    window.appData.userCount = totalClassifications;
+    console.log(totalClassifications);
+    $(document).ready(function() {
+      $(container).html(totalClassifications)
+    });
+
+    listenForClassifications();
+  });
+};
+
+function listenForClassifications() {
+  // This code runs each time a classification event comes down
+  // the panoptes pusher pipe
+  panoptesChannel.bind('classification', function(data) {
+     // console.log(data);
+     var pusherProject = data['project_id'];
+     var pusherUser = data['user_id'];
+
+     function updateCount() {
+       if (pusherProject === String(projectID) && pusherUser === String(userID)) {
+         window.appData.userCount++;
+         $(document).ready(function() {
+           $("#counter").html(window.appData.userCount);
+         });
+       };
+     };
+     updateCount();
   });
 }
 
-// Get current classifcation count
-getClassificationCount("http://stats.zooniverse.org/counts/classification/year?project_id=3434");
 
 
 
-$(document).ready(function() {
-  $("#counter").html('x');
-});
+//
+// step1_initialiseAppAndFetchStartingNumber('http://stats.zooniverse.org/counts/classification/year?project_id=4996&user_id=1804243', "#counter");
+//
+//
+//
+// var currentCount = document.getElementById("counter");
+// console.log(currentCount);
+//
+//
+//
+//
+// function step1_initialiseAppAndFetchStartingNumber(url, container) {
+//   $.getJSON(url, function(data) {
+//     var total_classifications = 0;
+//     var stats_data = data.events_over_time.buckets;
+//     for (var i in stats_data) {
+//       total_classifications = total_classifications + parseInt(stats_data[i].doc_count);
+//     };
+//     window.appData.count = total_classifications;
+//     $(document).ready(function(){
+//       $("#counter").html(total_classifications)
+//     });
+//
+//     step2_listenForFurtherAdditions();
+//   });
+// };
+//
+// function step2_listenForFurtherAdditions() {
+//   panoptesChannel.bind('classification', function(data) {
+//      // console.log(data);
+//
+//      var classified_project = data['project_id'];
+//
+//      function logSpecificUser() {
+//        if (classified_project === String(projectID)) {
+//          window.appData.count++;
+//          $(document).ready(function() {
+//            $("#counter").html(window.appData.count);
+//          });
+//        };
+//      };
+//      logSpecificUser();
+//   });
+// }
+//
 
-// This code runs each time a classification event comes down
-// the panoptes pusher pipe
 
-panoptesChannel.bind('classification', function(data) {
-   // console.log(data);
-
-   var classified_project = data['project_id'];
-
-   var currentCount = document.getElementById('id')
-
-   function logSpecificUser() {
-     if (classified_project === String(projectID)) {
-       Count = (Count + 1);
-       $(document).ready(function() {
-         $("#counter").html(Count);
-       });
-     };
-   };
-   logSpecificUser();
-});
+//
+// // This code runs each time a classification event comes down
+// // the panoptes pusher pipe
+// var currentCount = document.getElementById('counter');
+// console.log(currentCount);
+// panoptesChannel.bind('classification', function(data) {
+//    // console.log(data);
+//
+//    var classified_project = data['project_id'];
+//
+//    function logSpecificUser() {
+//      if (classified_project === String(projectID)) {
+//        Count = (Count + 1);
+//        $(document).ready(function() {
+//          $("#counter").html(Count);
+//        });
+//      };
+//    };
+//    logSpecificUser();
+// });
