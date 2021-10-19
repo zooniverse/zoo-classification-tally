ea0c26b130bbc91a02cb40305faa408613b4d0db
Campbell Allen <campbell.allen@gmail.com>
2021-10-19 17:49:04 +0100

    remove old deploy script

--- bin/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TALLY_PATH="./"
-DEPLOY_PATH="s3://zooniverse-static/tally.zooniverse.org/"
-aws s3 sync "${TALLY_PATH}" "${DEPLOY_PATH}" --delete --exclude "*" --include "main.*.js"  --include "index.html"
